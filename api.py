601b8bc9de64c62900326152ed52c6566704ea55
Dilva <Dilva@MacBook-Air-de-Dilva.local>
2020-02-07 16:48:51 +0100

    Correct request when empty arguments

--- api.py
+++ api.py
@@ -1,25 +1,36 @@
+"""
+API module
+"""
+
 from flask import Flask, request, render_template
 import database_access as dba
 
 
 app = Flask(__name__)
 @app.route('/')
-def func():
+def home_page():
     # Home page
     return render_template("index.html")
 
 
 @app.route('/catalog')
-def func1():
-    # Returns all objects
+def get_all_catalog():
+    # Returns all the objects in the catalog
     return dba.get_all_objects()
     
 
 @app.route('/objects', methods=['GET'])
-def func2():
+def filter_objects():
     # Gets entered arguments and returns all filtered objects
-    return dba.get_objects(['*'], request.args) 
+    arguments = request.args.copy()
+    # Removes all empty arguments
+    args_to_remove = [k for k,v in arguments.items() if v == ""]
+    for arg in args_to_remove:
+        arguments.pop(arg)
+    return dba.get_objects(['*'], arguments) 
     
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
